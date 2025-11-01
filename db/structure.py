eb71d86a81a8e6b5eee4301240b6800f7e75c084
kotvpalto <kotvpaltoof@ya.ru>
2025-11-01 17:55:09 +0300

    структура товаров и заказов

--- db/structure.py
+++ db/structure.py
@@ -1,15 +1,19 @@
 import sqlalchemy as sql
 import sqlalchemy.orm as orm
 from sqlalchemy.ext.hybrid import hybrid_property
-from datetime import datetime
 from datetime import datetime, timedelta
 from hashlib import sha256
+from typing import List
 
 engine = sql.create_engine('sqlite:///database.db')
 session = orm.Session(bind=engine)
 
 Base = orm.declarative_base()
 metadata: sql.MetaData = Base.metadata
+
+orders_products_junction = sql.Table("orders_products_junction_table", Base.metadata, 
+    sql.Column("order_id", sql.ForeignKey("orders.id"), primary_key=True),
+    sql.Column("product_id", sql.ForeignKey("products.id"), primary_key=True))
 
 class User(Base):
     __tablename__ = "users"
@@ -19,8 +23,9 @@ class User(Base):
 
     username: orm.Mapped[str] = orm.mapped_column(sql.Text, nullable=False, unique=True)
     _email: orm.Mapped[str] = orm.mapped_column(sql.Text, nullable=False, unique=True, name="email")
-
     _password_hash: orm.Mapped[str] = orm.mapped_column(sql.String(64), nullable=False, name="password_hash")
+    
+    orders: orm.Mapped[List["Order"]] = orm.relationship(back_populates="user")
     
     @hybrid_property
     def email(self):
@@ -65,4 +70,26 @@ class Token(Base):
     
     is_active: orm.Mapped[bool] = orm.mapped_column(sql.Boolean, nullable=False, default=True)
     
+class Product(Base):
+    __tablename__ = "products"
+
+    id: orm.Mapped[int] = orm.mapped_column(sql.Integer, primary_key=True)
+    
+    name: orm.Mapped[str] = orm.mapped_column(sql.Text, nullable=False)
+    description: orm.Mapped[str] = orm.mapped_column(sql.Text)
+    price: orm.Mapped[float] = orm.mapped_column(sql.Float, nullable=False, default=0)
+    
+    orders: orm.Mapped[List["Order"]] = orm.relationship(secondary=orders_products_junction, back_populates="products")
+     
+class Order(Base):
+    __tablename__ = "orders"
+    
+    id: orm.Mapped[int] = orm.mapped_column(sql.Integer, primary_key=True)
+    
+    products: orm.Mapped[List["Product"]] = orm.relationship(secondary=orders_products_junction, back_populates="orders")
+    
+    _user_id: orm.Mapped[int] = orm.mapped_column(sql.ForeignKey("users.id"))
+    user: orm.Mapped["User"] = orm.relationship(back_populates="orders")
+
+
 metadata.create_all(engine)
